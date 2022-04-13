ab59f2b06e42b5b8e5c5f3e77d514ebffdd1d16d
Pedro Rezende <pedrohar2021@gmail.com>
2022-04-13 19:55:27 -0300

    INSERT PROJECT

---
+++ projeto01.js
@@ -0,0 +1,105 @@
+console.clear();
+const prompt = require('prompt-sync')();
+
+let respostaSim = 0
+
+console.log('----------------------------------------');
+console.log('<<<- PROJETO 1 - A JORNADA DO HERÓI ->>>');
+console.log('----------------------------------------');
+console.log(' SUPER MARIO BROSS - DE VOLTA PRA CASA');
+console.log('----------------------------------------');
+console.log(`    O SUPER MARIO DEVE AJUDAR O LUIGI
+  EM UMA MISSÃO, E DEPOIS DE CONCLUIR... 
+    VOLTAR PRA CASA, QUE TAL AJUDA-LO?`);
+console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
+console.log();
+console.log('VAMOS LÁ...');
+console.log();
+console.log(`Nesse GAME, você é o personagem "Mario".
+Seu objetivo é ajudar o Liugi e voltar pra casa.
+Utilize respostas [S] ou [N] para as perguntas realizadas.
+A cada pergunta respondida você acumula pontos,
+No final veremos se seu objetivo foi concluido.`)
+console.log();
+
+// ----------------------------------------------------------------------- //
+
+console.log('<<- FASE 01 ->>');
+let perguntaUm = prompt('Mario BROSS, você foi ajudar na missão? [s/n]: ').toUpperCase()
+if (perguntaUm == 'S'){
+  console.log(`A resposta foi: ${perguntaUm}IM, você ganhou 1 ponto.`);
+  respostaSim ++
+}else{
+  console.log(`A resposta foi: ${perguntaUm} = ("NÃO"), você ganhou 0 pontos.`);
+}
+console.log();
+
+// ----------------------------------------------------------------------- //
+console.log('<<- FASE 02 ->>');
+let perguntaDois = prompt('Mario BROSS, conseguiram concluir a missão? [s/n]: ').toUpperCase()
+if (perguntaDois == 'S'){
+  console.log(`A resposta foi: ${perguntaDois}IM, você ganhou 1 ponto.`);
+  respostaSim ++
+}else{
+  console.log(`A resposta foi: ${perguntaDois} = ("NÃO"), você ganhou 0 pontos.`);
+}
+console.log();
+
+// ----------------------------------------------------------------------- //
+
+console.log('<<- FASE 03 ->>');
+let perguntaTrês = prompt('Mario BROSS, você se despediu de Luigi? [s/n]: ').toUpperCase()
+if (perguntaTrês == 'S'){
+  console.log(`A resposta foi: ${perguntaTrês}IM, você ganhou 1 ponto.`);
+  respostaSim ++
+}else{
+  console.log(`A resposta foi: ${perguntaTrês} = ("NÃO"), você ganhou 0 pontos.`);
+}
+console.log();
+
+// ----------------------------------------------------------------------- //
+
+console.log('<<- FASE 04 ->>');
+let perguntaQuatro = prompt('Mario BROSS, vamos voltar pra casa? [s/n]: ').toUpperCase()
+if (perguntaQuatro == 'S'){
+  console.log(`A resposta foi: ${perguntaQuatro}IM, você ganhou 1 ponto.`);
+  respostaSim ++
+}else{
+  console.log(`A resposta foi: ${perguntaQuatro} = ("NÃO"), você ganhou 0 pontos.`);
+}
+console.log();
+
+// ----------------------------------------------------------------------- //
+
+console.log('<<- FASE 05 ->>');
+let perguntaCinco = prompt('Mario BROSS, você chegou em casa? [s/n]: ').toUpperCase()
+if (perguntaCinco == 'S'){
+  console.log(`A resposta foi: ${perguntaCinco}IM, você ganhou 1 ponto.`);
+  respostaSim ++
+}else{
+  console.log(`A resposta foi: ${perguntaCinco} = ("NÃO"), você ganhou 0 pontos.`);
+}
+console.log();
+
+// ----------------------------------------------------------------------- //
+
+console.log('<<<- FIM DO JOGO! ->>> ');
+console.log();
+
+if (respostaSim == 0){
+console.log('      [0]PONTOS \nVocê falhou miseravelmente.');
+}else if (respostaSim == 1 ){
+  console.log('      [1]PONTO \nVocê falha, mas ainda consegue fazer algo.');
+}else if (respostaSim == 2 ){
+  console.log('      [2]PONTOS \nVocê falha, mas ainda consegue fazer algo.');
+}else if (respostaSim == 3){
+  console.log('      [3]PONTOS \nVocê chega perto de conseguir alcançar seu objetivo, mas acaba falhando por pouco.');
+}else if (respostaSim == 4){
+  console.log('      [4]PONTOS \nDepois de muito esforço você conquista seu objetivo, embora não de maneira perfeita.');
+}else if (respostaSim == 5){
+  console.log('      [5]PONTOS \nVocê triunfa de maneira inquestionável.');
+}
+
+console.log('----------------------------------------');
+
+
