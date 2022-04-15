c3aad1a0aca4568ff1ca8fc5ffe6110495a4ef65
Pedro Rezende <pedrohar2021@gmail.com>
2022-04-15 16:52:38 -0300

    UPDATE PROJECT

--- projeto01.js
+++ projeto01.js
@@ -26,34 +26,46 @@ console.log();
 
 console.log('<<- FASE 01 ->>');
 let perguntaUm = prompt('Mario BROSS, você foi ajudar na missão? [s/n]: ').toUpperCase()
+while (perguntaUm != 'S' && perguntaUm != 'N'){
+  console.log('Resposta Invalida.');
+  perguntaUm = prompt('Mario BROSS, você foi ajudar na missão? [s/n]: ').toUpperCase()
+}
 if (perguntaUm == 'S'){
   console.log(`A resposta foi: ${perguntaUm}IM, você ganhou 1 ponto.`);
   respostaSim ++
 }else{
-  console.log(`A resposta foi: ${perguntaUm} = ("NÃO"), você ganhou 0 pontos.`);
+  console.log(`A resposta foi: ${perguntaUm}ÃO, você ganhou 0 pontos.`);
 }
 console.log();
 
 // ----------------------------------------------------------------------- //
 console.log('<<- FASE 02 ->>');
 let perguntaDois = prompt('Mario BROSS, conseguiram concluir a missão? [s/n]: ').toUpperCase()
+while (perguntaDois != 'S' && perguntaDois != 'N'){
+  console.log('Resposta Invalida.');
+  perguntaDois = prompt('Mario BROSS, conseguiram concluir a missão? [s/n]: ').toUpperCase()
+}
 if (perguntaDois == 'S'){
   console.log(`A resposta foi: ${perguntaDois}IM, você ganhou 1 ponto.`);
   respostaSim ++
 }else{
-  console.log(`A resposta foi: ${perguntaDois} = ("NÃO"), você ganhou 0 pontos.`);
+  console.log(`A resposta foi: ${perguntaDois}ÃO, você ganhou 0 pontos.`);
 }
 console.log();
 
 // ----------------------------------------------------------------------- //
 
 console.log('<<- FASE 03 ->>');
-let perguntaTrês = prompt('Mario BROSS, você se despediu de Luigi? [s/n]: ').toUpperCase()
-if (perguntaTrês == 'S'){
-  console.log(`A resposta foi: ${perguntaTrês}IM, você ganhou 1 ponto.`);
+let perguntaTres = prompt('Mario BROSS, você se despediu de Luigi? [s/n]: ').toUpperCase()
+while (perguntaTres != 'S' && perguntaTres != 'N'){
+  console.log('Resposta Invalida.');
+  perguntaTres = prompt('Mario BROSS, você se despediu de Luigi? [s/n]: ').toUpperCase()
+}
+if (perguntaTres == 'S'){
+  console.log(`A resposta foi: ${perguntaTres}IM, você ganhou 1 ponto.`);
   respostaSim ++
 }else{
-  console.log(`A resposta foi: ${perguntaTrês} = ("NÃO"), você ganhou 0 pontos.`);
+  console.log(`A resposta foi: ${perguntaTres}ÃO, você ganhou 0 pontos.`);
 }
 console.log();
 
@@ -61,11 +73,15 @@ console.log();
 
 console.log('<<- FASE 04 ->>');
 let perguntaQuatro = prompt('Mario BROSS, vamos voltar pra casa? [s/n]: ').toUpperCase()
+while (perguntaQuatro != 'S' && perguntaQuatro != 'N'){
+  console.log('Resposta Invalida.');
+  perguntaQuatro = prompt('Mario BROSS, vamos voltar pra casa? [s/n]: ').toUpperCase()
+}
 if (perguntaQuatro == 'S'){
   console.log(`A resposta foi: ${perguntaQuatro}IM, você ganhou 1 ponto.`);
   respostaSim ++
 }else{
-  console.log(`A resposta foi: ${perguntaQuatro} = ("NÃO"), você ganhou 0 pontos.`);
+  console.log(`A resposta foi: ${perguntaQuatro}ÃO, você ganhou 0 pontos.`);
 }
 console.log();
 
@@ -73,11 +89,15 @@ console.log();
 
 console.log('<<- FASE 05 ->>');
 let perguntaCinco = prompt('Mario BROSS, você chegou em casa? [s/n]: ').toUpperCase()
+while (perguntaCinco != 'S' && perguntaCinco != 'N'){
+  console.log('Resposta Invalida.');
+  perguntaCinco = prompt('Mario BROSS, você chegou em casa? [s/n]: ').toUpperCase()
+}
 if (perguntaCinco == 'S'){
   console.log(`A resposta foi: ${perguntaCinco}IM, você ganhou 1 ponto.`);
   respostaSim ++
 }else{
-  console.log(`A resposta foi: ${perguntaCinco} = ("NÃO"), você ganhou 0 pontos.`);
+  console.log(`A resposta foi: ${perguntaCinco}ÃO, você ganhou 0 pontos.`);
 }
 console.log();
 
@@ -87,19 +107,21 @@ console.log('<<<- FIM DO JOGO! ->>> ');
 console.log();
 
 if (respostaSim == 0){
-console.log('      [0]PONTOS \nVocê falhou miseravelmente.');
+console.log('      [0]PONTOS \n\nVocê falhou miseravelmente.');
 }else if (respostaSim == 1 ){
-  console.log('      [1]PONTO \nVocê falha, mas ainda consegue fazer algo.');
+  console.log('      [1]PONTO \n\nVocê falha, mas ainda consegue fazer algo.');
 }else if (respostaSim == 2 ){
-  console.log('      [2]PONTOS \nVocê falha, mas ainda consegue fazer algo.');
+  console.log('      [2]PONTOS \n\nVocê falha, mas ainda consegue fazer algo.');
 }else if (respostaSim == 3){
-  console.log('      [3]PONTOS \nVocê chega perto de conseguir alcançar seu objetivo, mas acaba falhando por pouco.');
+  console.log('      [3]PONTOS \n\nVocê chega perto de conseguir alcançar seu objetivo, \nmas acaba falhando por pouco.');
 }else if (respostaSim == 4){
-  console.log('      [4]PONTOS \nDepois de muito esforço você conquista seu objetivo, embora não de maneira perfeita.');
+  console.log('      [4]PONTOS \n\nDepois de muito esforço você conquista seu objetivo, \nembora não de maneira perfeita.');
 }else if (respostaSim == 5){
-  console.log('      [5]PONTOS \nVocê triunfa de maneira inquestionável.');
+  console.log('      [5]PONTOS \n\nVocê triunfa de maneira inquestionável.');
 }
 
 console.log('----------------------------------------');
+
+
 
 
